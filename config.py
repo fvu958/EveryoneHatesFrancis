2250cc2b64df517141017674a3b9fa1d9b6de58b
Fvu <francis.nguyen.vu@gmail.com>
2018-06-30 10:45:53 -0700

    Let's do it

--- config.py
+++
@@ -1,6 +0,0 @@
-# These keys are not valid. Use your own API keys
-
-consumer_key = 'yBeoGt02oBSGj3WZtBLcQDqW5'
-consumer_secret = 'hC5YnVPIvMitGj5svP5JkNmzJySkOiB8PqnRFd4hhvCFeJMlOv'
-access_token = '879058918969810944-a4qMAcB2bakdJ7A9DycTKIAAHaGyP6O'
-access_token_secret = 'ilwmBxGa7LXNEiIlPbQbMo7HNmPXVGsaXs6lOM6p3GDbZ'
